a40de766b915fdfbe59dc095b148a4e53a18418f
Ben Vere <ben.vere@zoonou.com>
2013-09-09 17:13:45 +0100

    Updates
    Implemented dialog box
    String manipulation
    
    Now need to implement more text minipulation to remove spaces etc.

--- FoT_testBuild/FoT_testBuild/MainWindow.cs
+++ FoT_testBuild/FoT_testBuild/MainWindow.cs
@@ -8,8 +8,20 @@ public partial class MainWindow: Gtk.Window
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
+		onStartActions ();
+	
+	}
 
+	/*General start up parameters*/
+	protected void onStartActions(){
+		// Turn off visable labels and text areas
 		enabledSection = false;
+		labelClient2.Visible = false;
+		labelProject2.Visible = false;
+		entry3.Visible = false;
+		entry4.Visible = false;
+		enabledSection = false;
+		//---------------------------------------
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
@@ -26,23 +38,43 @@ public partial class MainWindow: Gtk.Window
 
 	protected void OnButtonContinueClicked (object sender, EventArgs e)
 	{
-		// temp solution until 'continue' is implemented
-		Gtk.Main.Quit (); // Quite main application
 
-		throw new NotImplementedException ();
+		string clientName1 = entry1.Text;
+		string projectName1 = entry2.Text;
+
+		if(clientName1 == ""){
+			clientName1 = "Client";
+		}
+
+		if(projectName1 == ""){
+			projectName1 = "Project";
+		}
+
+		string halfDayResource = clientName1 + "_" + projectName1;
+
+		//MessageDialog.
+		MessageDialog myDialogWindow = new MessageDialog(this, DialogFlags.Modal, MessageType.Error, ButtonsType.Ok, halfDayResource);
+		//Opens dialog
+		myDialogWindow.Run();
+		//Only one option 'close' - Will close dialog
+		myDialogWindow.Destroy();
 	}
-
+	 
 
 	protected void OnRadioResource2Toggled (object sender, EventArgs e)
 	{
+		//Change the display status of the below objects
+			//visable - on / off - disabled
 		if (enabledSection == false) {
 			labelClient2.Visible = true;
 			labelProject2.Visible = true;
 			entry3.Visible = true;
+			entry4.Visible = true;
 			enabledSection = true;
 		} else {
 			labelClient2.Visible = false;
 			labelProject2.Visible = false;
+			entry3.Visible = false;
 			entry4.Visible = false;
 			enabledSection = false;
 		}
