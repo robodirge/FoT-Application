0a31c332f50330721fbdd121942195dd5c3f5fbd
Ben Vere <ben.vere@zoonou.com>
2013-09-09 15:47:28 +0100

    Day 1
    update - early restart

--- FoT_testBuild/FoT_testBuild/MainWindow.cs
+++ FoT_testBuild/FoT_testBuild/MainWindow.cs
@@ -3,9 +3,13 @@ using Gtk;
 
 public partial class MainWindow: Gtk.Window
 {	
+	public static bool enabledSection { get; set; }
+
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
+
+		enabledSection = false;
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
@@ -13,4 +17,35 @@ public partial class MainWindow: Gtk.Window
 		Application.Quit ();
 		a.RetVal = true;
 	}
+
+	protected void OnButtonQuitClicked (object sender, EventArgs e)
+	{
+		Gtk.Main.Quit (); // Quite main application
+		throw new NotImplementedException ();
+	}
+
+	protected void OnButtonContinueClicked (object sender, EventArgs e)
+	{
+		// temp solution until 'continue' is implemented
+		Gtk.Main.Quit (); // Quite main application
+
+		throw new NotImplementedException ();
+	}
+
+
+	protected void OnRadioResource2Toggled (object sender, EventArgs e)
+	{
+		if (enabledSection == false) {
+			labelClient2.Visible = true;
+			labelProject2.Visible = true;
+			entry3.Visible = true;
+			enabledSection = true;
+		} else {
+			labelClient2.Visible = false;
+			labelProject2.Visible = false;
+			entry4.Visible = false;
+			enabledSection = false;
+		}
+	}
+
 }
