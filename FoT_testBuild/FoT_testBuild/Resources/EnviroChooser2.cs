cb4402d6502a4ab9ba677eae03368e3d712e025c
Ben Vere <ben.vere@zoonou.com>
2013-10-17 11:29:42 +0100

    Commit 17/10/2013 #1

--- FoT_testBuild/FoT_testBuild/Resources/EnviroChooser2.cs
+++ FoT_testBuild/FoT_testBuild/Resources/EnviroChooser2.cs
@@ -3,6 +3,9 @@ using Gtk;
 
 namespace FoT{
 	public partial class EnviroChooser2 : Gtk.Window{
+
+		static public bool isAllTypesActive;
+
 		public EnviroChooser2 () : 
 				base(Gtk.WindowType.Toplevel){
 			this.Build ();
@@ -19,6 +22,9 @@ namespace FoT{
 			//Gdk.Color c = new Gdk.Color();
 			//Gdk.Color.Parse("red", ref c);
 
+			isAllTypesActive = true;
+
+			enablelvl3(0);
 
 		}
 		//<param> Desktop button clicked </param>
@@ -165,14 +171,14 @@ namespace FoT{
 		}
 
 		//<param> enable visible lvl 3 items </param>
-		public void enablelvl3(int inumber){
+		public void enablelvl3(int inumber)
+		{
 			button9.Visible = true;
 			button10.Visible = true;
 			button11.Visible = true;
 			button12.Visible = true;
 			button13.Visible = true;
 			button14.Visible = true;
-			button15.Visible = true;
 			button16.Visible = true;
 			button17.Visible = true;
 			button18.Visible = true;
@@ -180,7 +186,6 @@ namespace FoT{
 			button20.Visible = true;
 			button21.Visible = true;
 			button22.Visible = true;
-			button23.Visible = true;
 			button24.Visible = true;
 			button25.Visible = true;
 			button26.Visible = true;
@@ -199,7 +204,6 @@ namespace FoT{
 				button12.Visible = false;
 				button13.Visible = false;
 				button14.Visible = false;
-				button15.Visible = false;
 				button16.Visible = false;
 				button17.Visible = false;
 				button18.Visible = false;
@@ -207,7 +211,6 @@ namespace FoT{
 				button20.Visible = false;
 				button21.Visible = false;
 				button22.Visible = false;
-				button23.Visible = false;
 				button24.Visible = false;
 				button25.Visible = false;
 				button26.Visible = false;
@@ -226,7 +229,6 @@ namespace FoT{
 				button12.Visible = false;
 				button13.Visible = false;
 				button14.Visible = false;
-				button15.Visible = false;
 				button16.Visible = false;
 				button17.Visible = false;
 				button18.Visible = false;
@@ -234,7 +236,8 @@ namespace FoT{
 				button20.Visible = false;
 				button21.Visible = false;
 				button22.Visible = false;
-				button23.Visible = false;
+
+				enableSelection3(inumber);
 			}
 			else{
 				button24.Visible = false;
@@ -247,7 +250,126 @@ namespace FoT{
 				button31.Visible = false;
 				button32.Visible = false;
 				button33.Visible = false;
+
+				enableSelection3(inumber);
 			}
+		}
+   
+		//<param> enable visible lvl 3 items </param> 24 -28
+		public void enableSelection3(int inumber){
+			switch(inumber){
+			case 3:
+				button29.Visible = false;
+				button30.Visible = false;
+				button31.Visible = false;
+				button32.Visible = false;
+				button33.Visible = false;
+				break;
+			case 4:
+				button24.Visible = false;
+				button25.Visible = false;
+				button26.Visible = false;
+				button27.Visible = false;
+				button28.Visible = false;
+				button33.Visible = false;
+				break;
+			case 5:
+				button24.Visible = false;
+				button25.Visible = false;
+				button26.Visible = false;
+				button27.Visible = false;
+				button28.Visible = false;
+				button29.Visible = false;
+				button30.Visible = false;
+				button31.Visible = false;
+				button32.Visible = false;
+				break;
+			case 6:
+				button16.Visible = false;
+				button17.Visible = false;
+				button18.Visible = false;
+				button19.Visible = false;
+				button20.Visible = false;
+				button21.Visible = false;
+			
+				//button22.Sensitive = isAllTypesActive;
+				button22.Click();
+
+				break;
+			case 7:
+				button11.Visible = false;
+				button12.Visible = false;
+				button13.Visible = false;
+				button14.Visible = false;
+
+
+				//button22.Sensitive = isAllTypesActive;
+				button22.Click();
+
+				break;
+			case 8:
+				button9.Visible = false;
+				button10.Visible = false;
+				button11.Visible = false;
+				button12.Visible = false;
+				button13.Visible = false;
+				button14.Visible = false;
+				button16.Visible = false;
+				button17.Visible = false;
+				button18.Visible = false;
+				button19.Visible = false;
+				button20.Visible = false;
+				button21.Visible = false;
+				button22.Visible = false;
+				break;
+			default:
+				break;
+			}
+		}
+
+		protected void OnButton22Clicked (object sender, EventArgs e){
+			if(isAllTypesActive == true){
+				button22.Sensitive = false;
+				//isAllTypesActive = false;
+				button9.Sensitive = true;
+				button10.Sensitive = true;
+				label1.Text = @"All Android devices // Or iOS // Or Other";
+			}
+			else{
+				return;
+			}
+		}
+
+		protected void OnButton9Clicked (object sender, EventArgs e){
+			isAllTypesActive = true;
+			button22.Sensitive = true;
+			button9.Sensitive = false;
+			button10.Sensitive = true;
+			label1.Text = @"Tablet devices";
+		}
+
+		protected void OnButton10Clicked (object sender, EventArgs e){
+			isAllTypesActive = true;
+			button22.Sensitive = true;
+			button9.Sensitive = true;
+			button10.Sensitive = false;
+			label1.Text = @"Mobile devices";
+		}
+
+
+		protected void OnButton11Clicked (object sender, EventArgs e){
+			enableAlllvl4();
+			button11.Sensitive = false;
+			label1.Text = @"Android v2.0 devices";
+		}
+
+		public void enableAlllvl4(){
+			button9.Sensitive = true;
+			button10.Sensitive = true;
+			button11.Sensitive = true;
+			button12.Sensitive = true;
+			button13.Sensitive = true;
+			button14.Sensitive = true;
 		}
 	}
 }
